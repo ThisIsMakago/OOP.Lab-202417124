d45476099294eb6eb2bb25820ceade6cea06796f
ThisIsMakago <phamhuuhieu28@gmail.com>
2026-04-16 20:42:20 +0700

    Fix Aims.java

--- Lab02/src/hust/soict/globalict/aims/Aims.java
+++ Lab02/src/hust/soict/globalict/aims/Aims.java
@@ -1,7 +1,7 @@
+package hust.soict.globalict.aims;
 import hust.soict.globalict.aims.cart.Cart;
 import hust.soict.globalict.aims.disc.DigitalVideoDisc;
 
-package hust.soict.globalict.aims;
 public class Aims {
     public static void main(String[] args) {
 
